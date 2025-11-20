0d131039a2250842f1b2b585196ca907872153b7
mahdiHafsi1 <mehdi.hafsi@rades.r-iset.tn>
2025-11-20 13:42:50 +0100

    Initial commit

---
+++ composeApp/src/iosMain/kotlin/org/example/fishingstore/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.example.fishingstore
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/webMain/kotlin/org/example/fishingstore/main.kt
@@ -0,0 +1,11 @@
+package org.example.fishingstore
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
